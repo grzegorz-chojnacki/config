42c68d60022d2e39e8ae5a8c34eafd6d971c5801
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-25 14:54:56 +0200

    Improved script

--- autolink.sh
+++ autolink.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-targets=(
-  "bashrc"
-  "bash_profile"
-  "gitconfig"
-  "inputrc"
-  "vim" # whole directory
-  "xinitrc"
-  "Xresources"
-)
-
 # Make sure that current working directory is the repo
 repo="$(dirname "$0")"
 cd $repo
 
+repoName=$(pwd)
+configPath=$HOME/.config
+
+if [[ $repoName != $configPath ]]; then
+  if [[ -e $HOME/.config ]]; then
+    echo "Another '.config' directory detected. Aborting."
+    exit 1
+  fi
+  mv $repoName $configPath
+fi
+
+targets=(
+  'bash/bashrc'
+  'bash/bash_profile'
+  'git/gitconfig'
+  'inputrc'
+  'vim'
+  'xorg/xinitrc'
+  'xorg/Xresources'
+)
+
 for target in ${targets[@]}; do
-  path=$(find -name $target -printf '%P\n')
-  echo $target "->" .config/$path
-  ln -sf .config/$path ~/.$target
+  echo ".config/$target -> $HOME/.${target##*/}"
+  ln -s .config/$target $HOME/.${target##*/}
 done
 
-[[ ${PWD##*/} != '.config' ]] && echo "Rename directory to '.config'"
-
+echo 'done.'
